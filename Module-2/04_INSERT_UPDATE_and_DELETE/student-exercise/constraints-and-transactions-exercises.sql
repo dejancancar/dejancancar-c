e9ab67ac7dbfc0e28a3755db8b770aca2d682994
Dejan Cancar <dejancancar84@gmail.com>
2021-02-11 19:54:02 -0500

    Week 5 Day 4 Homework Complete

--- Module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ Module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -2,45 +2,110 @@
 -- The following changes are applied to the "dvdstore" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+INSERT INTO actor (first_name, last_name)
+	VALUES ('Hampton', 'Avenue'), ('Lisa', 'Byway')
+
+SELECT * FROM actor
+
+SELECT @@IDENTITY
+--Hampton 201
+--Lisa 202
 
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
+INSERT INTO Film (title, description, release_year, language_id, length)
+	VALUES ('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in
+-- ancient Greece', 2008, 1, 198)
+
+SELECT * FROM film WHERE title = 'Euclidean PI'
+SELECT @@IDENTITY
+-- EUCLIDEAN PI - ID 1001
+
+SELECT * FROM film WHERE title = 'Euclidean PI'
 
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+INSERT INTO film_actor (actor_id, film_id)
+	VALUES (201, 1001), (202,1001)
+
+SELECT * FROM film_actor WHERE actor_id = 201
+SELECT @@IDENTITY
 
 -- 4. Add Mathmagical to the category table.
+INSERT INTO category (name)
+	VALUES ('Mathmagical')
+
+SELECT * FROM category WHERE name = 'Mathmagical'
+SELECT @@IDENTITY
+
+--Mathmagical - 17
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+INSERT INTO film_category (film_id, category_id)
+	VALUES ((SELECT film_id FROM film WHERE title = 'Euclidean PI'), (SELECT category_id FROM category WHERE name = 'Mathmagical'))
+INSERT INTO film_category (film_id, category_id)
+	VALUES ((SELECT film_id FROM film WHERE title = 'EGG IGBY'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+	((SELECT film_id FROM film WHERE title = 'KARATE MOON'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+	((SELECT film_id FROM film WHERE title = 'RANDOM GO'), (SELECT category_id FROM category WHERE name = 'Mathmagical')),
+	((SELECT film_id FROM film WHERE title = 'YOUNG LANGUAGE'), (SELECT category_id FROM category WHERE name = 'Mathmagical'))
+
+
+
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
 
+SELECT * FROM film where TITLE = 'egg igby'
+UPDATE film SET rating = 'G' WHERE FILm_id in(SELECT film_id FROM film_category WHERE category_id = (SELECT category_id FROM category WHERE name = 'Mathmagical'))
+
+SELECT * FROM film_category WHERE category_id = 17
+commit transaction
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+INSERT INTO inventory(film_id,store_id)
+	VALUES ((SELECT film_id FROM film WHERE title = 'Euclidean PI'),1),
+	((SELECT film_id FROM film WHERE title = 'Euclidean PI'),2)
+
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
+DELETE FROM film WHERE title = 'Euclidean PI'
+
 -- <YOUR ANSWER HERE>
+--Primary Key is referenced elsewhere by a foreign key so it is constrained and cannot be deleted
+	--The film_Id is connected to the catergory_id in the film_category table, actors_id in the film_actor table, and inventory_id in the store table.
+	--The error states that it is connected to the film_actor table through film_id
 
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
+DELETE FROM category WHERE name = 'Mathmagical'
 -- <YOUR ANSWER HERE>
+	--Same as above primary key is referenced by a foreign key in film_category
+	--Error says that its connected in the film_category through catergory_id
 
 -- 10. Delete all links to Mathmagical in the film_category tale.
 -- (Did it succeed? Why?)
+DELETE FROM film_category WHERE category_id = (SELECT category_id FROM category WHERE name = 'Mathmagical')
 -- <YOUR ANSWER HERE>
+	--we are deleting the forign key from the primary key of category so you are not constrained.
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
+DELETE FROM category WHERE name = 'Mathmagical'
+DELETE FROM film WHERE title = 'Euclidean PI'
 -- (Did either deletes succeed? Why?)
 -- <YOUR ANSWER HERE>
+	--the primary key in the category table is not referenced anywhere else so Mathmagical is able to be deleted.
+	--The film_id (primary key) is still referenced in the inventory table and actor_film tables.
+
 
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
+	--The only other constraint seems to be on the film_actor table, so to remove Euclidean PI from film you would first have to remove the reference in film_actor
+	
--- Module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture-ms sql.sql
+++ Module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture-ms sql.sql
@@ -1,39 +1,99 @@
 -- INSERT
 -- 1. Add a city to the City table, district 'Ohio'
--- 1a. What is the @@Identity?
--- 2. Add 2 more cities to Ohio using one insert statement
--- 2a. What is the @@Identity?
+INSERT INTO City (Name, CountryCode, District, Population)
+	VALUES ('Dejanville', 'USA', 'Ohio', 1969);
 
--- 0. Add Klingon to the Language table
+	
+-- 1a. What is the @@Identity?
+SELECT @@IDENTITY
+
+-- 2. Add 2 more cities to Ohio using one insert statement
+INSERT INTO City (Name, CountryCode, District,Population)
+	VALUES 
+	('Cleveland Heigths', 'USA', 'Ohio', 43000),
+	('Lakewood', 'USA', 'Ohio', 50000)
+-- 2a. What is the @@Identity?
+SELECT @@IDENTITY
+
+-- 0. Add Klingon to the Language table (ADDED 458)
+INSERT INTO Language (LanguageName)
+	VALUES ('Klingon')
 -- 0a. What is the @@Identity?
+SELECT @@IDENTITY
 -- 1. Add Klingon as a spoken language in the USA
+INSERT INTO CountryLanguage (CountryCode, LanguageId, IsOfficial, Percentage)
+	VALUES ('USA', 458, 0, 12.0)
+
+		-- Languages of the USA:
+			SELECT *
+				FROM CountryLanguage cl
+				JOIN Language l ON cl.LanguageId = l.LanguageId
+				WHERE cl.CountryCode = 'USA'
 -- 2. Add Klingon as a spoken language in Great Britain
+INSERT INTO CountryLanguage (CountryCode, LanguageId, IsOfficial, Percentage)
+	VALUES ( 'GBR', 458, 1, 32.0)
+
+	INSERT INTO CountryLanguage (CountryCode, LanguageId, IsOfficial, Percentage)
+	VALUES ( 'GBR', (SELECT LanguageId from Language WHERE  LanguageName = 'Klingon'), 1, 32.0)
+
+select * from Country
+	WHERE name like '%brit%'
 
 
 -- UPDATE
 
 -- 0. Update the population of Cleveland to 2020 values (379,233)
--- 1. Update the capital of the USA to Houston
--- 2. Update the capital of the USA to Washington DC and the head of state
+UPDATE City
+	SET Population = 379233 WHERE name = 'Cleveland'
+	-- check with cityID or primary key incase more than 1 cleveland
+	--PUT WHERE in the same line as SET to make sure you grab both
+	
 
+
+-- 1. Update the capital of the USA to Houston
+--NEED TO KNOW CITY ID first
+SELECT * 
+	FROM City
+	WHERE name = 'Houston' AND District = 'Texas'
+
+SELECT * 
+	FROM Country 
+	WHERE Code = 'USA'
+
+UPDATE Country
+	SET Capital = (SELECT CityID FROM City WHERE name = 'Houston' AND District = 'Texas')
+	WHERE Code = 'USA'
+
+-- 2. Update the capital of the USA to Washington DC and the head of state
+UPDATE Country
+	SET Capital = (SELECT CityID FROM City WHERE name = 'Washington'), HeadOfState = 'Joe Biden' WHERE Code = 'USA'
 
 -- DELETE
 
 -- 0. Delete the newly added Ohio cities
+DELETE FROM City WHERE name = 'Dejanville' AND District = 'Ohio'
+
+DELETE FROM City WHERE name in ('Cleveland Heigths' , 'Lakewood' , 'Dejanville') AND District = 'Ohio'
+
 -- 1. Delete English as a spoken language in the USA
+DELETE FROM CountryLanguage WHERE CountryCode = 'USA' AND LanguageId = (SELECT LanguageId FROM Language WHERE LanguageName = 'Klingon')
 -- 2. Delete all occurrences of the Klingon language 
+DELETE FROM CountryLanguage WHERE LanguageId = (SELECT LanguageId FROM Language WHERE LanguageName = 'Klingon')
+DELETE FROM Language WHERE LanguageName ='Klingon'
 
 
 -- REFERENTIAL INTEGRITY
 
 -- 1. Add a city to the City table in the country 'ZZZ'
+INSERT INTO City (Name, CountryCode, District, Population)
+	VALUES ('Richfield', 'ZZZ', 'Ohio', 12000) -- error because country does not have country.code of ZZZ
 
 -- 1. Try just adding Elvish to the country language table.
 
 -- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
 
 -- 3. Try deleting the country USA. What happens?
-
+DELETE Country WHERE Code = 'USA'
 
 -- CONSTRAINTS
 
@@ -42,6 +102,9 @@
 -- 2. Try again. What happens?
 
 -- 3. Let's relocate the USA to the continent - 'Outer Space'
+UPDATE Country 
+	SET Continent = 'Outer Space' -- not in continent so you cannot update USA to outer space.
+	WHERE Code = 'USA'
 
 
 -- How to view all of the constraints
@@ -54,6 +117,10 @@ SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS
 -- TRANSACTIONS
 
 -- 1. Try deleting all of the rows from the country language table and roll it back.
+BEGIN TRANSACTION
+	DELETE FROM CountryLanguage
+	SELECT * FROM CountryLanguage
+ROLLBACK TRANSACTION
 
 -- 2. Try updating all of the cities to be in the USA and roll it back
 
