68281cf4497f7dea749a1cfc8e40c96c29d3cc8c
Dejan Cancar <dejancancar84@gmail.com>
2021-03-16 17:05:06 -0400

    week 10 day 2 update repo

--- Module-3/07_JavaScript_Functions/student-lecture/lecture.js
+++ Module-3/07_JavaScript_Functions/student-lecture/lecture.js
@@ -7,7 +7,8 @@ function arrayFunctions()
     let phrase1 = "When in the course of human events it becomes necessary for one people to dissolve the political bands";
     // split here...
 
-    let words;
+    let words = phrase1.split(" ")
+    printArray(words);
     // call printArray here...
     
 
@@ -15,7 +16,7 @@ function arrayFunctions()
     // array.slice(startElement, nonInclusiveEndElement)
     console.log('*** ***\r\narray.slice gets a "sub-array" but does not modify the original' );
     // slice here...
-    let arr;
+    let arr = words.slice(3, 7);
     printArray(arr);
     printArray(words);
     
@@ -24,6 +25,7 @@ function arrayFunctions()
     // array.splice(start, count, newelements…)
     console.log('*** ***\r\narray.splice gets a "sub-array" and removes those elements.' );
     // splice here...
+    arr = words.splice(3, 4, " middle", "of", "the")
     printArray(arr);
     printArray(words);
 
@@ -32,7 +34,7 @@ function arrayFunctions()
     // array.concat joins two arrays, and does not modify either.
     console.log('*** ***\r\narray.concat joins two arrays, and does not modify either.' );
     // concat here...
-    let bigArray;
+    let bigArray = words.concat(arr);
     printArray(words);
     printArray(arr);
     printArray(bigArray);
@@ -60,7 +62,9 @@ function printArray(arr){
  * @param {number} firstParameter the first parameter to multiply
  * @param {number} secondParameter the second parameter to multiply
  */
-
+function multiplyTogether(num1, num2){
+  return num1 * num2;
+}
 /**
  * This version makes sure that no parameters are ever missing. If
  * someone calls this function without parameters, we default the
@@ -71,16 +75,32 @@ function printArray(arr){
  * @param {number} [firstParameter=0] the first parameter to multiply
  * @param {number} [secondParameter=1] the second parameter to multiply
  */
-
+function multiplyTogether(num1 = 0, num2 = 0){
+  return num1 * num2;
+}
 
 /**
  * How can I write the multiply function so that it will multiply any number of parameters?
  */
+function multiplyTogether(){
+  if(arguments.length === 0){
+    return 0;
+  }
+  let product = 1;
+  for (arg of arguments){
+    if(typeof(arg) === "number"){
+        product *= arg;  
+    }
+
+  }
+  return product;
+}
 
 
 // How can we change this so that it can add up zero to three numbers?
 
 /**
+ * THis function adds three numbers together
  * 
  * @param {number} num1 A number to add
  * @param {number} num2 A number to add
@@ -119,49 +139,68 @@ function doubleIt(n){
  * There is actually a "variable" called doubleIt now
  */
 // print it...
-
+console.log(`doubleIt is a type ${typeof(doubleIt)}`)
 
 
 /************************
  * Now that the function is defined, we can actually "assign" that function to 
  * another variable.
  */
-
+let f = doubleIt;
+console.log(`f is a type ${typeof(f)}`);
 
 // Call the function doubleIt
-
-
+let answer = doubleIt(14);
+console.log(`The answer is ${answer}`);
 
 // Call the function f
-
+answer = f(100);
+console.log(`The new answer is ${answer}`)
 
 
 /*****************************
  * Another way to define a function - assign it to a variable directly
  * 
  */
+let tripleIt = function(n) {
+  return n*3;
+}
 
+answer = tripleIt(333);
+console.log(`The tripleIt answer is ${answer}`)
 
 
 /*******************************
  * And finally, a shortcut for the above using lambda (fat arrow)
  * 
  */
+let quadrupleIt = (n) => {
+  return n*4;
+}
 
+answer = quadrupleIt(400);
+console.log(`The quadrupleIt answer is ${answer}`)
 
 /************************************
  * You may even see a shorter-cut, called an expression-bodied function
  * but I won't use it normally
  */
 
-
+let quintupleIt = (n) => n*5;
  
 /************************************
  * Now we can write a function, which takes another function as a parameter.
  * The passed-in function can be called (executed / invoked).
+ * The return value will be a new array with the same number of elements as the original,
+ * but each element modified by the functionToApply
  */
 function toAllElements(array, functionToApply){
+  let resultArray = [];
 
+  for (ele of array){
+    resultArray.push(functionToApply(ele))
+  }
+  return resultArray;
 }
 
 /***********************************
@@ -193,8 +232,13 @@ let myArray = [1,2,3,4,5];
  * @returns {number[]} a new array with only those numbers that are
  *   multiples of 3
  */
-function allDivisibleByThree(numbersToFilter) {}
-
+function allDivisibleByThree(numbersToFilter) {
+  // return numbersToFilter.filter(isNumberDivisibleByThree);
+  return numbersToFilter.filter((n) => { return n % 3 === 0})
+}
+function isNumberDivisibleByThree(n){
+  return n % 3 === 0;
+}
 /***************************************************************** */
 /********************* Array Functions  ************************** */
 /***************************************************************** */
@@ -214,25 +258,54 @@ function sumAllNumbers(numbersToSum) {
 
 
 let people = [
-  { name: "name", age: 21, height: 71 },
+  { name: "Brandon", age: 29, height: 67 },
+  { name: "Kameron", age: 20, height: 69 },
+  { name: "Robin", age: 29, height: 76 },
+  { name: "Jess", age: 23, height: 77 },
+  { name: "Paul", age: 25, height: 67 },
 ];
 
 // List all the people using forEach
 function listAllPeople(people) {
+  people.forEach( (person) => { 
+    console.log(person.name)
+  });
 }
 
 // Filter people by height or age
 function shortPeople(people) {
+  return people.filter((n) => { 
+    return n.height < 72; 
+  });
 }
 
 // Map Name to uppercase
 function upperName(people) {
+  return people.map( (person) => {
+    // return `${person.name.toUpperCase()} (${person.age})`;
+    return {Name: person.name, BirthYear: 2021-person.age};
+  });
 }
 
 
 // Reduce to total height of all people
 function totalHeight(people) {
+  return people.reduce((sum, person) => { 
+    return sum + person.height;
+  }, 0 )
 }
 
 // Sort the array
-
+/**
+ * Fuction to sort people by age, descending
+ * @param {array} people The array to sort
+ * @returns A sorted array of people by height
+ */
+function sortPeople(people){
+  return people.sort( (p1, p2) => {
+    // if p1 > p2, return a positive value
+    // if p1 < p2, return a negative value
+    // if ===, return 0
+    return p1.height - p2.height;
+  });
+}
